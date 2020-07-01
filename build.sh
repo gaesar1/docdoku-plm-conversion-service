7339029ecc07d4cda8f36964b1e30eb3eaa41cee
Morgan Guimard <morgan.guimard@docdoku.com>
2020-07-01 09:03:03 +0200

    remove unused docker image file+clean pom

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --build-arg USERID=$(id -u) --build-arg GROUPID=$(id -g) -f Dockerfile.cpp -t docdokuplm/openjdk8-gcc . && \
+#docker build --build-arg USERID=$(id -u) --build-arg GROUPID=$(id -g) -f Dockerfile.cpp -t docdokuplm/openjdk8-gcc . && \
 mvn clean install && \
 docker build -f Dockerfile.jvm -t docdoku/docdoku-plm-conversion-service .
